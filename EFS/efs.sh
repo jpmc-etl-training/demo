ccb328c60de90e036c4ba50a27faf1b78e0653a8
retheshnair <retheshnair@users.noreply.github.com>
2021-08-05 19:19:21 +0530

    Update efs.sh

--- EFS/efs.sh
+++ EFS/efs.sh
@@ -5,3 +5,12 @@ sudo yum –y install nfs-utils
 
 #Ubuntu 
 sudo apt-get –y install nfs-common
+
+
+#Creating the EFS using aws cli
+aws efs create-file-system --performance-mode generalPurpose --throughput-mode bursting --encrypted --tags env=dev,Value=my-test-filesystem
+
+#Checking for EFS filesystem status
+aws efs describe-file-systems --file-system-id FILESYSTEM_NAME
+
+https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/efs.html
