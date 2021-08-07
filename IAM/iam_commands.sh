60a2efbef722fb14595d462d6ced1a51194c0eb3
retheshnair <retheshnair@gmail.com>
2021-08-07 10:01:42 +0530

    Update iam_commands.sh

--- IAM/iam_commands.sh
+++ IAM/iam_commands.sh
@@ -2,3 +2,4 @@ aws iam create-user --user-name rethesh
 aws iam create-group --group-nam admins
 aws iam add-user-to-group --user-name rethesh --group-name admins
 aws iam attach-group-policy --policy-arn arn:aws:iam::aws:policy/AdministratorAccess --group-name admins
+aws iam create-login-profile --user-name rethesh --password XXXXXXX --password-reset-required
