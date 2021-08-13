b7b147f7e7623d11a072673c192eb124c3de67fa
retheshnair <retheshnair@users.noreply.github.com>
2021-08-13 06:51:06 +0530

    Update demo2.sql

--- RedShift/demo2.sql
+++ RedShift/demo2.sql
@@ -1,7 +1,5 @@
 create schema if not exists s3_copy;
 
-drop table s3_copy.users;
-
 create table s3_copy.users(
     userid integer not null distkey sortkey,
     username char(8),
@@ -75,33 +73,33 @@ create table s3_copy.sales(
     commission decimal(8,2),
     saletime timestamp);
 
-copy s3_copy.users from 's3://rettestbucket/tickit/allusers_pipe.txt' 
+copy s3_copy.users from 's3://awssampledbuswest2/tickit/allusers_pipe.txt' 
 credentials 'aws_iam_role=arn:aws:iam::187493414600:role/myRedshiftRole' 
-delimiter '|' region 'us-east-1';
-                            
-copy s3_copy.venue from 's3://awssampledbuseast1/tickit/venue_pipe.txt' 
+delimiter '|' region 'us-west-2';
+
+copy s3_copy.venue from 's3://awssampledbuswest2/tickit/venue_pipe.txt' 
 credentials 'aws_iam_role=arn:aws:iam::187493414600:role/myRedshiftRole' 
-delimiter '|' region 'us-east-1';
+delimiter '|' region 'us-west-2';
 
 copy s3_copy.category from 's3://awssampledbuswest2/tickit/category_pipe.txt' 
 credentials 'aws_iam_role=arn:aws:iam::187493414600:role/myRedshiftRole' 
-delimiter '|' region 'us-east-1';
+delimiter '|' region 'us-west-2';
 
 copy s3_copy.date from 's3://awssampledbuswest2/tickit/date2008_pipe.txt' 
 credentials 'aws_iam_role=arn:aws:iam::187493414600:role/myRedshiftRole' 
-delimiter '|' region 'us-east-1';
+delimiter '|' region 'us-west-2';
 
 copy s3_copy.event from 's3://awssampledbuswest2/tickit/allevents_pipe.txt' 
 credentials 'aws_iam_role=arn:aws:iam::187493414600:role/myRedshiftRole' 
-delimiter '|' timeformat 'YYYY-MM-DD HH:MI:SS' region 'us-east-1';
+delimiter '|' timeformat 'YYYY-MM-DD HH:MI:SS' region 'us-west-2';
 
 copy s3_copy.listing from 's3://awssampledbuswest2/tickit/listings_pipe.txt' 
 credentials 'aws_iam_role=arn:aws:iam::187493414600:role/myRedshiftRole' 
-delimiter '|' region 'us-east-1';
+delimiter '|' region 'us-west-2';
 
 copy s3_copy.sales from 's3://awssampledbuswest2/tickit/sales_tab.txt'
 credentials 'aws_iam_role=arn:aws:iam::187493414600:role/myRedshiftRole'
-delimiter '\t' timeformat 'MM/DD/YYYY HH:MI:SS' region 'us-east-1';
+delimiter '\t' timeformat 'MM/DD/YYYY HH:MI:SS' region 'us-west-2';
 
 select * from s3_copy.users limit 10;
 
